7c7886939f08871d4ecbefefd60a128bb5c095c6
Brijen Makwana <brijenmakwana@192.168.1.7>
2022-11-16 16:39:23 +0530

    rules

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ const house_choice_container = document.getElementById("house-choice");
 const user_choice_image = document.getElementById("user-choice-image");
 const house_choice_image = document.getElementById("house-choice-image");
 
+const game_result_container = document.getElementById("game-result-container");
+
 const game_result_text = document.getElementById("game-result-text");
 const play_again_btn = document.getElementById("play-again-btn");
 
-const bodyContainer = document.getElementsByTagName("body");
+const rule_container_btn = document.getElementById("rule-container");
+const overlay_container = document.getElementById("overlay-container");
+const close_rules = document.getElementById("close-rules");
 
 const availableMoves = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -37,64 +41,73 @@ allOptions.forEach((option) => {
   });
 });
 
+// house move display
+
 const houseMove = () => {
   const randomNumber = Math.floor(Math.random() * availableMoves.length);
   houseChoice = availableMoves[randomNumber];
   // display result
+
   house_choice_container.classList.add(`user-${houseChoice}`);
   house_choice_image.src = `./images/icon-${houseChoice}.svg`;
 
-  getResult();
+  setTimeout(() => {
+    getResult();
+  }, 1500);
 };
 
+const userWinnerEffect = () => {
+  game_result_text.innerHTML = "you win";
+  score++;
+  user_choice_container.classList.add("winner");
+};
+
+const houseWinnerEffect = () => {
+  game_result_text.innerHTML = "you lose";
+  house_choice_container.classList.add("winner");
+  score--;
+};
+
+// result display
 const getResult = () => {
+  game_result_container.style.display = "flex";
   if (userChoice === houseChoice) {
     game_result_text.innerHTML = "it's a draw";
   } else {
     switch (userChoice) {
       case "rock":
         if (houseChoice === "scissors" || houseChoice === "lizard") {
-          game_result_text.innerHTML = "you win";
-          score++;
+          userWinnerEffect();
         } else {
-          game_result_text.innerHTML = "you lose";
-          score--;
+          houseWinnerEffect();
         }
         break;
       case "paper":
         if (houseChoice === "rock" || houseChoice === "Spock") {
-          game_result_text.innerHTML = "you win";
-          score++;
+          userWinnerEffect();
         } else {
-          game_result_text.innerHTML = "you lose";
-          score--;
+          houseWinnerEffect();
         }
         break;
       case "scissors":
         if (houseChoice === "paper" || houseChoice === "lizard") {
-          game_result_text.innerHTML = "you win";
-          score++;
+          userWinnerEffect();
         } else {
-          game_result_text.innerHTML = "you lose";
-          score--;
+          houseWinnerEffect();
         }
         break;
       case "lizard":
         if (houseChoice === "paper" || houseChoice === "spock") {
-          game_result_text.innerHTML = "you win";
-          score++;
+          userWinnerEffect();
         } else {
-          game_result_text.innerHTML = "you lose";
-          score--;
+          houseWinnerEffect();
         }
         break;
       case "spock":
         if (houseChoice === "rock" || houseChoice === "scissors") {
-          game_result_text.innerHTML = "you win";
-          score++;
+          userWinnerEffect();
         } else {
-          game_result_text.innerHTML = "you lose";
-          score--;
+          houseWinnerEffect();
         }
         break;
     }
@@ -102,11 +115,24 @@ const getResult = () => {
     localStorage.setItem("stored_score", score);
   }
 };
+
+// reset everything to play again
 const playAgain = () => {
   bg_pentagon.style.display = "block";
   selected_options_container.style.display = "none";
   user_choice_container.classList.remove(`user-${userChoice}`);
   house_choice_container.classList.remove(`user-${houseChoice}`);
+  user_choice_container.classList.remove("winner");
+  house_choice_container.classList.remove("winner");
+  game_result_container.style.display = "none";
 };
 
 play_again_btn.addEventListener("click", playAgain);
+
+rule_container_btn.addEventListener("click", () => {
+  overlay_container.style.display = "block";
+});
+
+close_rules.addEventListener("click", () => {
+  overlay_container.style.display = "none";
+});
